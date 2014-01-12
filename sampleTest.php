f8756533bb4c73d3705d0f1ef9a66de3ae228b86
mbraux <martial@braux.net>
2014-01-12 20:22:51 +0100

    Sample test

---
+++ sampleTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Sample test for PHPSimpleUnitTestsFramework.
+ * @author martial@braux.net
+ * See the LICENSE file for legal details.
+ */
+class MyClassTest extends PHPSimpleUnitTestsFramework {
+  
+  function testMyFirstMethod() {
+    $myVar = 'This is a test';
+    $this->assertIsset('myVar should be set.', $myVar);
+    $this->assertEquals('The value should be correct.', 'This is a test', $myVar);
+  }
+  
+  function testMySecondMethod() {
+    $myVar = array('Another test');
+    $this->assertIsArray('myVar should be an array.', $myVar);
+  }
+
+}
+
+$tests = new MyClassTest();
+$tests->run();
+
+?>
